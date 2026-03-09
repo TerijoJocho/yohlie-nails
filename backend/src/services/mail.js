d3d039d46871ba9a36b7da2d834d889b0ea5478c
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-03-09 19:07:20 +0100

    feat: migrate email sending from Resend to Brevo service and update email handling

--- backend/src/services/mail.js
+++ backend/src/services/mail.js
@@ -1,23 +1,29 @@
-import { Resend } from "resend";
+import * as Brevo from "@getbrevo/brevo";
+import dotenv from "dotenv";
+dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
 
 export async function sendBookingEmail({ client_name, client_email, client_choices, date, time }) {
   const choicesList = client_choices?.length > 0
     ? client_choices.join(", ")
     : "Aucune prestation sélectionnée";
 
-  await resend.emails.send({
-    from: "Yohlie Nails <onboarding@resend.dev>",
-    to: client_email,
-    subject: "Confirmation de réservation",
-    text: `Bonjour ${client_name}, votre réservation est confirmée pour le ${date} à ${time}.\n\nPrestation(s) : ${choicesList}\n\nMerci,\nYohlie Nails.`,
-  });
+  const emailClient = new Brevo.SendSmtpEmail();
+  emailClient.subject = "Confirmation de réservation";
+  emailClient.to = [{ email: client_email, name: client_name }];
+  emailClient.sender = { name: "Yohlie Nails", email: process.env.ADMIN_EMAIL };
+  emailClient.textContent = `Bonjour ${client_name},\n\nVotre réservation est confirmée pour le ${date} à ${time}.\n\nPrestation(s) : ${choicesList}\n\nMerci pour votre confiance,\nYohlie Nails.`;
 
-  await resend.emails.send({
-    from: "Yohlie Nails <onboarding@resend.dev>",
-    to: process.env.ADMIN_EMAIL,
-    subject: "Nouvelle réservation",
-    text: `${client_name} a réservé le ${date} à ${time}.\nPrestation(s) : ${choicesList}`,
-  });
+  const emailAdmin = new Brevo.SendSmtpEmail();
+  emailAdmin.subject = "Nouvelle réservation";
+  emailAdmin.to = [{ email: process.env.ADMIN_EMAIL }];
+  emailAdmin.sender = { name: "Yohlie Nails", email: process.env.ADMIN_EMAIL };
+  emailAdmin.textContent = `Client:${client_name} \n E-mail:(${client_email}) \na réservé le ${date} à ${time}.\nPrestation(s) : ${choicesList}`;
+
+  await Promise.all([
+    apiInstance.sendTransacEmail(emailClient),
+    apiInstance.sendTransacEmail(emailAdmin),
+  ]);
 }
